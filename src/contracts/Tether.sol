09cbefe7a2deb4d4e40408e2dfcd453f46752f0e
Darshan <darshanshah.ds2017@gmail.com>
2023-11-13 11:19:04 +0545

    migration part 1

---
+++ src/contracts/Tether.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.0;
+
+contract Tether {
+    string name = 'Tether';
+    string symbol = 'USDT';
+    uint256 totalSupply = 1000000000000000000000000; // 1 million tokens
+    uint8 decimals = 18;
+}
+
--- src/contracts/migrations.sol
+++ src/contracts/migrations.sol
@@ -1,24 +1,24 @@
-pragma solidity '^0.5.0';
+pragma solidity ^0.5.0;
 
 contract Migrations {
     address public owner;
     uint public last_completed_migration;
 
     constructor() public {
-        owner = msg.sender;
+        owner = msg.sender; //msg.sender means the current person who is currently connecting with the contract in solidity
     }
 
     modifier restricted() {
         if(msg.sender == owner) _;
     }
 
-    function set_completed(uint completed) public restricted {
+    function setCompleted(uint completed) public restricted {
         last_completed_migration = completed;
     }
 
     function upgrade(address new_address) public restricted {
         Migrations upgraded = Migrations(new_address);
-        upgraded.set_completed(last_completed_migration);
+        upgraded.setCompleted(last_completed_migration);
     }
 
 }
